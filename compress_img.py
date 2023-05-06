1dfc782b7522066843ec7e21762112fa3f706702
Srejon Khan <srejonkhan79@gmail.com>
2023-05-06 23:21:01 +0600

    feat: update key name of tinypng api

--- compress_img.py
+++ compress_img.py
@@ -8,12 +8,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument("image_path", help="path to the image file")
 args = parser.parse_args()
 
-api_key = api_key_management.get_api_key("tinypng")
+api_key = api_key_management.get_api_key("tinify")
 
 if api_key == "":
     print("TinyPNG API Key - ")
-    api_key_management.set_api_key("tinypng", input())
-    api_key = api_key_management.get_api_key("tinypng")
+    api_key_management.set_api_key("tinify", input())
+    api_key = api_key_management.get_api_key("tinify")
 
 tinify.key = api_key
 
